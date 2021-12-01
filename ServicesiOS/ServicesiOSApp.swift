6aa3f81b5c583767de63114fa503d3a56565cb1c
JHON ESTEBAN MUNAR GOMEZ <munar117@hotmail.com>
2021-12-01 18:25:45 -0500

    project initial

---
+++ ServicesiOS/ServicesiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesiOSApp.swift
+//  ServicesiOS
+//
+//  Created by Jhon Munar on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ServicesiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
